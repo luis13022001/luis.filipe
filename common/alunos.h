80c117ad470aaa8962d7e293c9043786bee078bd
luis13022001 <72235382+luis13022001@users.noreply.github.com>
2020-10-31 19:18:47 -0300

    att

---
+++ common/alunos.h
@@ -0,0 +1,9 @@
+#ifndef ALUNO
+
+struct aluno{
+  int matricula;
+  char nome[30];
+  float n1,n2,n3;
+};
+
+#endif 
---
+++ stack/linkedstack.h
@@ -0,0 +1,15 @@
+#define SUCCESS 0 
+#define INVALID_NULL_POINTER -1 //sempre que a função receber null
+#define OUT_OF_MEMORY -2 
+#define OUT_OF_RANGE -3 
+#define ELEM_NOT_FOUND -4
+#define ERRO_SORTED -5
+#include "../common/alunos.h"
+
+
+typedef struct linkedstack Stack;
+
+Stack *create_stack(){
+    Stack *stack;
+    stack = 
+}
